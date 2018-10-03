3ddfc6b4fc50a8c67693826472cd393eab75b69a
dokechin <ttatsumi@ra2.so-net.ne.jp>
2018-10-04 08:03:17 +0900

    button menu

--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,25 @@
 <template>
   <section class="container">
-    <div class="row">
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-        <a
-          href="/quiz/index"
-          class="button--grey">スタート</a>
+    <h2 class="aligh-center">難読大陸</h2>
+    <div class="row justify-content-center">
+      <div class="col-2 align-items-center">
+        <a class="btn btn-primary" href="/quiz/index" role="button">スタート</a>
       </div>
     </div>
     <div class="row">
-      <div class="col-sm">
-        One of three columns
-      </div>
-      <div class="col-sm">
-        One of three columns
-      </div>
-      <div class="col-sm">
-        One of three columns
+      <br>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-2 lign-items-center">
+        <a class="btn btn-primary" href="/collection/index" role="button">コレクション</a>
       </div>
     </div>
-    <div class="media">
-      <div class="media-body">
-        <h5 class="mt-0">Media heading</h5>
-        <button type="button" class="btn btn-primary">Primary</button>
-        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+    <div class="row">
+      <br>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-2 lign-items-center">
+        <a class="btn btn-primary" href="/usage/index" role="button">遊び方</a>
       </div>
     </div>
   </section>
