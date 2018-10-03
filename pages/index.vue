3093fae7a7ddeb34703a3c23603f526fd0fcdd7e
dokechin <ttatsumi@ra2.so-net.ne.jp>
2018-10-04 07:34:20 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <section class="container">
+    <div class="row">
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          class="button--green">Documentation</a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey">GitHub</a>
+        <a
+          href="/quiz/index"
+          class="button--grey">スタート</a>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm">
+        One of three columns
+      </div>
+      <div class="col-sm">
+        One of three columns
+      </div>
+      <div class="col-sm">
+        One of three columns
+      </div>
+    </div>
+    <div class="media">
+      <div class="media-body">
+        <h5 class="mt-0">Media heading</h5>
+        <button type="button" class="btn btn-primary">Primary</button>
+        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+</style>
