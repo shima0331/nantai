38ab3ac889657c80149f06ebbb23d6fcd69190d8
dokechin <ttatsumi@ra2.so-net.ne.jp>
2018-10-04 22:09:20 +0900

    change button size

--- pages/index.vue
+++ pages/index.vue
@@ -2,24 +2,24 @@
   <section class="container">
     <h2 class="aligh-center">難読大陸</h2>
     <div class="row justify-content-center">
-      <div class="col-2 align-items-center">
-        <a class="btn btn-primary" href="/quiz/index" role="button">スタート</a>
+      <div class="col-4 align-items-center">
+        <a class="btn btn-primary btn-block" href="/quiz/index" role="button">スタート</a>
       </div>
     </div>
     <div class="row">
       <br>
     </div>
     <div class="row justify-content-center">
-      <div class="col-2 lign-items-center">
-        <a class="btn btn-primary" href="/collection/index" role="button">コレクション</a>
+      <div class="col-4 lign-items-center">
+        <a class="btn btn-primary btn-block" href="/collection/index" role="button">コレクション</a>
       </div>
     </div>
     <div class="row">
       <br>
     </div>
     <div class="row justify-content-center">
-      <div class="col-2 lign-items-center">
-        <a class="btn btn-primary" href="/usage/index" role="button">遊び方</a>
+      <div class="col-4 lign-items-center">
+        <a class="btn btn-primary btn-block" href="/usage/index" role="button">遊び方</a>
       </div>
     </div>
   </section>
