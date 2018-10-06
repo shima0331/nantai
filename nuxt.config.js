f8fdf6bb8a61282919ad06c5d49470abc8b013cd
dokechin <ttatsumi@ra2.so-net.ne.jp>
2018-10-07 03:16:49 +0900

    add deploy script

--- nuxt.config.js
+++ nuxt.config.js
@@ -71,3 +71,14 @@ module.exports = {
     }
   }
 }
+/* nuxt.config.js */
+// only add `router.base = '/<repository-name>/'` if `DEPLOY_ENV` is `GH_PAGES`
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
+  router: {
+    base: '/nantai/'
+  }
+} : {}
+
+export default {
+  ...routerBase
+}
