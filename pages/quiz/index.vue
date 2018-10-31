1118becf5d37f433689e1f1dfe655708dbbcde35
dokechin <ttatsumi@ra2.so-net.ne.jp>
2018-11-01 07:58:07 +0900

    path

--- pages/quiz/index.vue
+++ pages/quiz/index.vue
@@ -1,12 +1,12 @@
 <template>
   <section class="container">
-    <img id="map" src="/japanmap/JapanMap.png" usemap="#JapanMap" alt="" class="col-12" height="667px">
+    <img id="map" src="japanmap/JapanMap.png" usemap="#JapanMap" alt="" class="col-12" height="667px">
     <map id="JapanMap" name="JapanMap">
       <area :href="remainCount(22) > 0 ? '/quiz/pref/22' : 'javascript:void(0)' " title="静岡" shape="rect" coords="391,455,445,495"
             alt="静岡" @mouseover="remainCount(22) > 0 ? cImg(22) : cImg(99)" @mouseout="cImg(99)">
       <area :href="remainCount(14) > 0 ? '/quiz/pref/14' : 'javascript:void(0)' " title="神奈川" shape="rect" coords="447,454,512,486"
             alt="神奈川" @mouseover="remainCount(14) > 0 ? cImg(14) : cImg(99)" @mouseout="cImg(99)">
-      <area :href="remainCount(0) > 0 ? '/quiz/ref/0' : 'javascript:void(0)' " title="ランダム" shape="circle" coords="82,262,36" 
+      <area :href="remainCount(0) > 0 ? '/quiz/pref/0' : 'javascript:void(0)' " title="ランダム" shape="circle" coords="82,262,36" 
             alt="ランダム" @mouseover="remainCount(0) > 0 ? cImg(0) : cImg(99)" @mouseout="cImg(99)">
     </map>
     <div class="row"><br></div>
@@ -21,7 +21,7 @@ export default {
   methods: {
     cImg: function(id) {
       var imgEle = document.getElementById('map')
-      imgEle.src = '/japanmap/JapanMap' + id + '.png'
+      imgEle.src = 'japanmap/JapanMap' + id + '.png'
     },
     remainCount: function(pref_id) {
       var unanswereds = this.$store.state.quizzes.filter(
