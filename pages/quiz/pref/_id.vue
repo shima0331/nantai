7686a66f27f44c9f0e033032104a644b0283ef66
TaroSugimoto <44189111+TaroSugimoto@users.noreply.github.com>
2018-11-05 10:46:59 +0900

    最新ソース反映

--- pages/quiz/pref/_id.vue
+++ pages/quiz/pref/_id.vue
@@ -1,34 +1,65 @@
 <template>
   <section class="container">
+    <div class="answer">
+      <div class="row mb-2">
+        <div class="col"/>
+        <div class="col-10 text-center question-text">
+          <span v-if="type==0">この地名の読み方、わかるかな</span><span v-if="type==1"><span class="correct-text">正解！</span>カードをゲットしました</span><span v-if="type==2"><span class="incorrect-text">不正解！</span>もう一度答えてみよう</span>
+        </div>
+        <div class="col"/>
+      </div>
+      <div v-if="type==0 || type==2" class="row text-center">
+        <div class="col"/>
+        <div class="col-6 location-panel mb-2">
+          <div>
+            <img :src="questions[question_index].map_img" > 
+          </div>
+          {{ questions[question_index].pref }}{{ questions[question_index].city }}<span class="town-text">{{ questions[question_index].town }}</span>
+        </div>
+        <div class="col"/>
+      </div>
+      <div v-if="type==0 || type==2" class="row text-center mb-2">
+        <div class="col"/>
+        <div class="col-6 justify-content-md-center">
+          <b-form inline class="justify-content-center" @submit.prevent="answer">
+            <b-input v-model="yomi" placeholder="ここに入力してね" class="answer-input w-50 mr-2" />
+            <b-button type="submit" class="answer-btn badge-pill"><span v-if="type==0">回答</span><span v-if="type==2">再回答</span></b-button>
+          </b-form>
+        </div>
+        <div class="col"/>
+      </div>
+      <div v-if="type==0 || type==2" class="row clearfix">
+        <div class="col"/>
+        <div class="col-5 text-right">
+          <b-button class="other-btn badge-pill" @click="next">別の問題に挑戦</b-button>
+        </div>
+        <div class="col-5">
+          <b-button class="hint-btn badge-pill" @click="hint">ヒントをみる</b-button>
+        </div>
 
-    <div class="row">
-      <div class="col-12 clearfix">
-        <div class="float-left"><img src="logo.png" alt="Nandoku Tairiku"></div>
-        <div class="col-xs-6">
-          <h2>この地名の読み方、わかるかな</h2> 
+        <div class="col"/>
+      </div>
+      <div v-if="type==1">
+        <div class="row justify-content-center">
+          <div class="col-1"/>
+          <div class="col-5">
+            <Card :id="card.id" :pref="card.pref" :city="card.city" :town="card.town" :map_img="card.map_img" :spot="card.spot" :spot_img="card.spot_img" :spot_guide="card.spot_guide"/>
+          </div>
+          <div class="col-6">
+            <div v-if="questions.length != (question_index + 1)">        
+              <b-button @click="next">別の問題をとく</b-button>
+            </div>
+          </div>
         </div>
       </div>
+      <br>
     </div>
-    <div v-if="type == 2">
-      不正解
-    </div> 
-    <div v-if="type　== 0 || type == 2">
-      {{ questions[question_index].city }}<b>{{ questions[question_index].town }}</b>
-      <input v-model="yomi"><button @click="answer"><span v-if="type==0">回答</span><span v-if="type==2">再回答</span></button>
-    </div>
-    <div v-if="type　== 1">
-      正解！カードをゲットしました
-      <div class="row justify-content-center">
-        <div class="col-4">
-          <Card :id="card.id"/>
-        </div>
+    <div class="row mt-2">
+      <div class="col-6"/>
+      <div class="text-right">
+        <nuxt-link v-if="type!=1" to="/quiz" class="return-btn btn badge-pill">都道府県選択へ戻る</nuxt-link>
+        <nuxt-link v-if="type==1" to="/" class="return-btn btn badge-pill">トップへ戻る</nuxt-link>
       </div>
-      <div v-if="questions.length != (question_index + 1)">        
-        <button @click="next">別の問題をとく</button>
-      </div>
-      <nuxt-link :to="{name: 'quiz'}">
-        <button>戻る</button>
-      </nuxt-link>
     </div>
   </section>
 </template>
@@ -50,10 +81,10 @@ export default {
     }
   },
   fetch({ store, params }) {
-    store.dispatch('shuffle', { pref: params.id })
+    store.dispatch('shuffle', { pref_id: params.id })
   },
   methods: {
-    answer: function(event) {
+    answer: function() {
       // `this` inside methods point to the Vue instance
       var question = this.questions[this.question_index]
       if (question.town_yomi == this.yomi) {
@@ -64,9 +95,10 @@ export default {
         this.$store.commit('setCards', this.$store.state.cards)
       } else {
         this.type = 2
+        this.yomi = ''
       }
     },
-    next: function(event) {
+    next: function() {
       this.question_index++
       this.type = 0
       this.yomi = ''
@@ -76,4 +108,40 @@ export default {
 </script>
 
 <style>
+.answer {
+  background-color: #efffff;
+} /* 左右余白 */
+.location-panel {
+  background-color: #ffffff;
+}
+.question-text {
+  font-size: 20px;
+  color: #534741;
+}
+.incorrect-text {
+  color: #036a9e;
+}
+.correct-text {
+  color: #e06a3b;
+}
+.town-text {
+  font-size: 30px;
+  color: #e06a3b;
+} /* 左右余白 */
+.answer-input,
+.answer-input:focus {
+  outline: 0;
+  border-width: 2px;
+  border-color: #036a9e;
+}
+.answer-btn {
+  color: white;
+  background-color: #036a9e;
+} /* 左右余白 */
+.other-btn,
+.hint-btn,
+.return-btn {
+  color: white;
+  background-color: #1e98b9;
+} /* 左右余白 */
 </style>
