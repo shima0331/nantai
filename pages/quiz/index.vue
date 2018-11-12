3881e2cb7d8a9541e5ccc23b6d1b04c45e5ef8df
TaroSugimoto <44189111+TaroSugimoto@users.noreply.github.com>
2018-11-12 09:27:36 +0900

    鈴木さんのデータクリアの動作修正

--- pages/quiz/index.vue
+++ pages/quiz/index.vue
@@ -1,74 +1,55 @@
 <template>
   <section class="container">
+    <img id="map" src="japanmap/JapanMap.png" usemap="#JapanMap" alt="" class="col-12" height="667px">
+    <map id="JapanMap" name="JapanMap">
+      <nuxt-link v-if="remainCount(22) > 0" to="/quiz/pref/22">
+        <area title="静岡" shape="rect" coords="391,455,445,495"
+              alt="静岡" @mouseover="remainCount(22) > 0 ? cImg(22) : cImg(99)" @mouseout="cImg(99)">
+      </nuxt-link>
+      <nuxt-link v-if="remainCount(14) > 0" to="/quiz/pref/14">
+        <area title="神奈川" shape="rect" coords="447,454,512,486"
+              alt="神奈川" @mouseover="remainCount(14) > 0 ? cImg(14) : cImg(99)" @mouseout="cImg(99)">
+      </nuxt-link>
+      <nuxt-link v-if="remainCount(0) > 0" to="/quiz/pref/0">
+        <area title="ランダム" shape="circle" coords="82,262,36" 
+              alt="ランダム" @mouseover="remainCount(0) > 0 ? cImg(0) : cImg(99)" @mouseout="cImg(99)">
+      </nuxt-link>
+    </map>
+    <div class="row"><br></div>
     <div class="row">
-      <br>
-    </div>
-    <div class="mx-auto text-center">
-      <img src="logo.png" alt="Nandoku Tairiku" class="img-thumnail">
-    </div>
-    <div class="row">
-      <br>
-    </div>
-    <div class="row justify-content-center">
-      <div class="col-4 align-items-center">
-        <nuxt-link :class="remainCount(14) > 0 ? 'btn btn-primary btn-block' : 'btn btn-primary btn-block disabled'" to="/quiz/pref/14" role="button" disabled="remainCount(14) == 0">神奈川県</nuxt-link>
-      </div>
-    </div>
-    <div class="row">
-      <br>
-    </div>
-    <div class="row justify-content-center">
-      <div class="col-4 lign-items-center">
-        <nuxt-link :class="remainCount(22) > 0 ? 'btn btn-primary btn-block' : 'btn btn-primary btn-block disabled'" to="/quiz/pref/22" role="button" disabled="remainCount(22) == 0">静岡県</nuxt-link>
-      </div>
-    </div>
-    <div class="row">
-      <br>
-    </div>
-    <div class="row justify-content-center">
-      <div class="col-4 lign-items-center">
-        <nuxt-link :class="remainCount(0) > 0 ? 'btn btn-primary btn-block' : 'btn btn-primary btn-block disabled'" to="/quiz/pref/0" role="button">ランダム</nuxt-link>
-      </div>
-    </div>
-    <div class="row">
-      <br>
-    </div>
-    <div class="row justify-content-center">
-      <div class="col-4 lign-items-center">
-        <nuxt-link class="btn btn-primary btn-block" to="/" role="button">メニューへ</nuxt-link>
-      </div>
-    </div>
-    <div class="row">
-      <br>
-    </div>
-    <div class="row justify-content-center">
-      <div class="col-4 lign-items-center">
-        <button class="btn btn-primary btn-block" @click="clear">データクリア</button>
-      </div>
+      <button class="btn btn-primary " valign="top" @click="clear">データクリア</button>
     </div>
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
   methods: {
-    remainCount: function(id) {
+    cImg: function(id) {
+      var imgEle = document.getElementById('map')
+      imgEle.src = 'japanmap/JapanMap' + id + '.png'
+    },
+    remainCount: function(pref_id) {
       var unanswereds = this.$store.state.quizzes.filter(
         quiz =>
-          (id == 0 || id == quiz.pref) &&
-          !this.$store.state.cards.includes(quiz.id)
+          (pref_id == 0 || pref_id == quiz.pref_id) &&
+          !this.$store.state.cards.find(function(e) {
+            return e.id == quiz.id
+          })
       )
       return unanswereds.length
     },
     clear: function() {
-      this.$store.dispatch('saveCards', [])
-      this.$store.commit('setCards', [])
+      this.$store.dispatch('ClearCards', [])
+      this.$store.commit('ClearCards', [])
     }
   }
 }
 </script>
 
 <style>
+.col-12 {
+  flex: 0 0 100%;
+  max-width: 617px;
+}
 </style>
