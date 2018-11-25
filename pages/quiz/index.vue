e7853006c68e213a4bc0efd7415a3861e22da5a2
dokechin <ttatsumi@ra2.so-net.ne.jp>
2018-11-25 20:10:41 +0900

    resonsive imagemap

--- pages/quiz/index.vue
+++ pages/quiz/index.vue
@@ -1,20 +1,24 @@
 <template>
   <section class="container">
-    <img id="map" src="japanmap/JapanMap.png" usemap="#JapanMap" alt="" class="col-12" height="667px">
-    <map id="JapanMap" name="JapanMap">
-      <nuxt-link v-if="remainCount(22) > 0" to="/quiz/pref/22">
-        <area title="静岡" shape="rect" coords="391,455,445,495"
-              alt="静岡" @mouseover="remainCount(22) > 0 ? cImg(22) : cImg(99)" @mouseout="cImg(99)">
-      </nuxt-link>
-      <nuxt-link v-if="remainCount(14) > 0" to="/quiz/pref/14">
-        <area title="神奈川" shape="rect" coords="447,454,512,486"
-              alt="神奈川" @mouseover="remainCount(14) > 0 ? cImg(14) : cImg(99)" @mouseout="cImg(99)">
-      </nuxt-link>
-      <nuxt-link v-if="remainCount(0) > 0" to="/quiz/pref/0">
-        <area title="ランダム" shape="circle" coords="82,262,36" 
-              alt="ランダム" @mouseover="remainCount(0) > 0 ? cImg(0) : cImg(99)" @mouseout="cImg(99)">
-      </nuxt-link>
-    </map>
+    <div class="row">
+      <div class="col map-parent">
+        <img id="map" src="japanmap/JapanMap.png" usemap="#image-map" alt="">
+        <map name="image-map">
+          <nuxt-link v-if="remainCount(22) > 0" to="/quiz/pref/22">
+            <area title="静岡" shape="rect" coords="391,455,445,495"
+                  alt="静岡" @mouseover="remainCount(22) > 0 ? cImg(22) : cImg(99)" @mouseout="cImg(99)">
+          </nuxt-link>
+          <nuxt-link v-if="remainCount(14) > 0" to="/quiz/pref/14">
+            <area title="神奈川" shape="rect" coords="447,454,512,486"
+                  alt="神奈川" @mouseover="remainCount(14) > 0 ? cImg(14) : cImg(99)" @mouseout="cImg(99)">
+          </nuxt-link>
+          <nuxt-link v-if="remainCount(0) > 0" to="/quiz/pref/0">
+            <area title="ランダム" shape="circle" coords="82,262,36" 
+                  alt="ランダム" @mouseover="remainCount(0) > 0 ? cImg(0) : cImg(99)" @mouseout="cImg(99)">
+          </nuxt-link>
+        </map>
+      </div>
+    </div>
     <div class="row"><br></div>
     <div class="row">
       <button class="btn btn-primary " valign="top" @click="clear">データクリア</button>
@@ -23,7 +27,39 @@
 </template>
 
 <script>
+import $ from 'jquery'
 export default {
+  created() {
+    $(function() {
+      var image_is_loaded = false
+      $('#map').on('load', function() {
+        if (!image_is_loaded) {
+          $('area').each(function() {
+            $(this).data('coords', $(this).attr('coords'))
+          })
+          image_is_loaded = true
+        }
+      })
+
+      function ratioCoords(coords, ratio) {
+        var coord_arr = coords.split(',')
+        for (var i = 0; i < coord_arr.length; i++) {
+          coord_arr[i] = Math.round(ratio * coord_arr[i])
+        }
+        return coord_arr.join(',')
+      }
+      $(window).on('resize', function() {
+        if (image_is_loaded) {
+          var img = $('#map')
+          var ratio = img.width() / 617
+
+          $('area').each(function() {
+            $(this).attr('coords', ratioCoords($(this).data('coords'), ratio))
+          })
+        }
+      })
+    })
+  },
   methods: {
     cImg: function(id) {
       var imgEle = document.getElementById('map')
@@ -48,8 +84,12 @@ export default {
 </script>
 
 <style>
-.col-12 {
-  flex: 0 0 100%;
+#map {
+  width: 100%;
+  height: auto;
+}
+.map-parent {
   max-width: 617px;
+  max-height: 667px;
 }
 </style>
