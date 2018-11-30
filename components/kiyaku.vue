76664fdce65ad75a33c9701c0529e837792086f0
TaroSugimoto <44189111+TaroSugimoto@users.noreply.github.com>
2018-11-30 09:15:30 +0900

    小針さんの修正版利用規約ポップアップ

---
+++ components/kiyaku.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class = "modalk">
+    動作環境 Google Chrome 70.0.0 以上<br><br>
+    難読大陸は、以下の著作物を改変して利用しています。<br><br>
+    公共クラウド（総務省）、観光情報、CCライセンス 表示 2.1 日本<br>
+    （http://creativecommons.org/licenses/by/2.1/jp/）、<br>
+    公共クラウド利用規約<br>
+    （https://www.chiikinogennki.soumu.go.jp/k-cloud-api/code/）<br>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
